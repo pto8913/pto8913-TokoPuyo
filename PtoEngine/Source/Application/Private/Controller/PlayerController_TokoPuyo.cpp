60091589850c30c4e86e8111ee68f3217ffe6a12
pto8913 <nn3kskyou@gmail.com>
2023-09-09 09:22:05 +0900

    add sounds

--- PtoEngine/Source/Application/Private/Controller/PlayerController_TokoPuyo.cpp
+++ PtoEngine/Source/Application/Private/Controller/PlayerController_TokoPuyo.cpp
@@ -1,8 +1,6 @@
 
 #include "Controller/PlayerController_TokoPuyo.h"
 
-#include "Actor/Character/Player.h"
-
 #include "Core/DirectX.h"
 
 PlayerController_TokoPuyo::PlayerController_TokoPuyo(DirectX11& dx)
